754851cbeb6b7b3222b66cf70b0a7efc2a345753
IanYang46 <eric86710@gmail.com>
2026-06-30 18:48:59 +0800

    app.py

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ def init_db():
         cursor.execute("INSERT OR IGNORE INTO settings VALUES ('exchange_rate', 4.5)")
         
         # 5. 建立預設 Admin 帳號
+        cursor.execute("UPDATE users SET password = ? WHERE username = ?", (admin_pw, admin_user))
         cursor.execute("INSERT OR REPLACE INTO users VALUES (?, ?, 'Admin')", (admin_user, admin_pw))
         
         # 6. 初始化全新權限矩陣 (對應您新增的模組)
